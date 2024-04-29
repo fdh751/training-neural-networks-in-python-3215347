cd657cf49759e361dcba58a37752a5b43c9f6cd8
fdh751 <168194492+fdh751@users.noreply.github.com>
2024-04-29 14:05:06 +0000

    first try

--- src/MLP.py
+++ src/MLP.py
@@ -120,21 +120,8 @@ class MultiLayerPerceptron:
 #test code
 
 
-# Dataset for the 7 to 1 network
-for i in range(epochs):
-    mse = 0.0
-    mse += mlp.bp([1,1,1,1,1,1,0],[0.05])    #0 pattern
-    mse += mlp.bp([0,1,1,0,0,0,0],[0.15])    #1 pattern
-    mse += mlp.bp([1,1,0,1,1,0,1],[0.25])    #2 pattern
-    mse += mlp.bp([1,1,1,1,0,0,1],[0.35])    #3 pattern
-    mse += mlp.bp([0,1,1,0,0,1,1],[0.45])    #4 pattern
-    mse += mlp.bp([1,0,1,1,0,1,1],[0.55])    #5 pattern
-    mse += mlp.bp([1,0,1,1,1,1,1],[0.65])    #6 pattern
-    mse += mlp.bp([1,1,1,0,0,0,0],[0.75])    #7 pattern
-    mse += mlp.bp([1,1,1,1,1,1,1],[0.85])    #8 pattern
-    mse += mlp.bp([1,1,1,1,0,1,1],[0.95])    #9 pattern
-    mse = mse/10.0
-
+mlp = MultiLayerPerceptron(layers={7,7,10})
+epochs = 2000
 
 # Dataset for the 7 to 10 network
 for i in range(epochs):
@@ -152,17 +139,4 @@ for i in range(epochs):
     mse = mse/10.0
 
 
-# Dataset for the 7 to 7 network
-for i in range(epochs):
-    mse = 0.0
-    mse += mlp.bp([1,1,1,1,1,1,0],[1,1,1,1,1,1,0])    #0 pattern
-    mse += mlp.bp([0,1,1,0,0,0,0],[0,1,1,0,0,0,0])    #1 pattern
-    mse += mlp.bp([1,1,0,1,1,0,1],[1,1,0,1,1,0,1])    #2 pattern
-    mse += mlp.bp([1,1,1,1,0,0,1],[1,1,1,1,0,0,1])    #3 pattern
-    mse += mlp.bp([0,1,1,0,0,1,1],[0,1,1,0,0,1,1])    #4 pattern
-    mse += mlp.bp([1,0,1,1,0,1,1],[1,0,1,1,0,1,1])    #5 pattern
-    mse += mlp.bp([1,0,1,1,1,1,1],[1,0,1,1,1,1,1])    #6 pattern
-    mse += mlp.bp([1,1,1,0,0,0,0],[1,1,1,0,0,0,0])    #7 pattern
-    mse += mlp.bp([1,1,1,1,1,1,1],[1,1,1,1,1,1,1])    #8 pattern
-    mse += mlp.bp([1,1,1,1,0,1,1],[1,1,1,1,0,1,1])    #9 pattern
-    mse = mse/10.0
+print(mlp.run([1,1,1,1,0,1,1]))
